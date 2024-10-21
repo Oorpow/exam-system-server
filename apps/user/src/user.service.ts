c5ab40f9ec5d0bd3275dd5f694bc5c66cf95ad1b
Oorpow <309754513@qq.com>
2024-10-21 11:27:02 +0800

    chore: 🐳 添加prisma支持，并共享服务

--- apps/user/src/user.service.ts
+++ apps/user/src/user.service.ts
@@ -1,7 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@app/prisma';
+import { Inject, Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UserService {
+  @Inject(PrismaService)
+  private prisma: PrismaService;
+
+  async create(data: Prisma.UserCreateInput) {
+    return await this.prisma.user.create({
+      data,
+      select: {
+        id: true,
+      },
+    });
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
