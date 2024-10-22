8d41f4d7465218584032678915f14e4d35ded0ff
Oorpow <309754513@qq.com>
2024-10-22 10:03:51 +0800

    refactor: 🦄 抽离JwtModule至common模块公用，添加exam表，补充authGuard注释

--- libs/common/src/auth.guard.ts
+++ libs/common/src/auth.guard.ts
@@ -49,6 +49,7 @@ export class AuthGuard implements CanActivate {
         secret: 'astrox',
       });
       request['user'] = payload;
+      // 每次调用需要登录的接口时，自动刷新token实现续签
       response.header(
         'token',
         this.jwtService.sign(
