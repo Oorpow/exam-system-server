e72344fc15f1930e8e1c52abeee6a8c0c06277b7
Oorpow <309754513@qq.com>
2024-10-22 10:04:59 +0800

    refactor: 🦄 改变jwt的引用方式，统一更改为引用commonModule

--- apps/user/src/user.module.ts
+++ apps/user/src/user.module.ts
@@ -2,25 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { PrismaModule } from '@app/prisma';
-import { JwtModule } from '@nestjs/jwt';
 import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from '@app/common';
+import { AuthGuard, CommonModule } from '@app/common';
 
 @Module({
-  imports: [
-    PrismaModule,
-    JwtModule.registerAsync({
-      global: true,
-      useFactory() {
-        return {
-          secret: 'astrox',
-          signOptions: {
-            expiresIn: '30m',
-          },
-        };
-      },
-    }),
-  ],
+  imports: [PrismaModule, CommonModule],
   controllers: [UserController],
   providers: [
     UserService,
